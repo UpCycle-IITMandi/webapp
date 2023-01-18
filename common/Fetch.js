78d844612866705b7d3544392f5a3991c20386a0
= <awesomepranshu@gmail.com>
2023-01-18 16:42:32 +0530

    Environment changes

--- common/Fetch.js
+++ common/Fetch.js
@@ -1,6 +1,9 @@
-const baseurl = "http://localhost:3000";
+// require('dotenv').config()
+const baseurl = process.env.BACKEND_URL
+
 function Fetch(props) {
   console.log("Sending request : ", props);
+  console.log("Base url" , baseurl)
   return fetch(baseurl + props.route, {
     headers:props.header,
     method: props.type,
